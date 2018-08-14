a804dbb2a54d65da634a31998cdd80210706dac1
Your Name <you@example.com>
2018-08-14 23:40:46 +0300

    change names and fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,25 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { BookComponent } from './book/book.component';
 import { ProductComponent } from './product/product.component';
+import { BooksComponent } from './books/books.component';
 
-const appRoutes: Routes = [
-  {
-    path: 'books',
-    component: BookComponent,
-    data: { title: 'Book List' }
-  },
-  { 
-    path: '',
-    redirectTo: '/books',
-    pathMatch: 'full'
-  },
-  {
-    path: 'products',
-    component: ProductComponent
-  }
-];
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent,
-    ProductComponent
+    BooksComponent,
+    ProductComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+  books: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.books = window['books'];
+    // this.http.get('/api/books').subscribe(books => {
+    //   this.books = books;
+    // })
+  }
+}
